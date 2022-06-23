02ec94ffabbce6c9342106ac5f2d6b3039b9dae7
b-khadija <khadija.bayssir@gmail.com>
2022-06-23 03:21:03 +0200

    Add images videos

--- src/travel.js
+++ src/travel.js
@@ -1,8 +1,37 @@
-var burgerMenu = document.getElementById('burger-menu');
+const menuToggle = document.querySelector(".menu-toggle");
+const navigation = document.querySelector(".navigation-list");
 
-var overlay = document.getElementById('menu');
+menuToggle.addEventListener("click", () => {
+  menuToggle.classList.toggle("active");
+  navigation.classList.toggle("active");
+});
 
-burgerMenu.addEventListener('click', function() {
-  this.classList.toggle("close");
-  overlay.classList.toggle("overlay");
+const buttons = document.querySelectorAll(".nav-slider-btn");
+const slides = document.querySelectorAll(".slider-video");
+const contents = document.querySelectorAll(".content")
+
+
+
+var navigationSlider = function(manual){
+  buttons.forEach((button) => {
+    button.classList.remove("active");
+  });
+
+  slides.forEach((slide) => {
+    slide.classList.remove("active");
+  });
+
+  contents.forEach((content) => {
+    content.classList.remove("active");
+  });
+
+  buttons[manual].classList.add("active");
+  slides[manual].classList.add("active");
+  contents[manual].classList.add("active");
+}
+
+buttons.forEach((button, i) => {
+  button.addEventListener("click", () => {
+    navigationSlider(i);
+  });
 });
